5660e867aa3774628b8426ad3805244594df97d3
nishabi <616387521@qq.com>
2018-04-03 15:13:01 +0800

    自动化框架

--- 自动化框架/login126.py
+++ 自动化框架/login126.py
@@ -1,14 +1,21 @@
 # -*- coding:utf-8 -*-
 from selenium import webdriver
 import time
-
-driver = webdriver.Chrome()
-driver.get("http://mail.163.com")
-time.sleep(3)
 def login():
     frame = driver.find_element_by_xpath("//*[@id='x-URS-iframe']")
     driver.switch_to.frame(frame)
     driver.find_element_by_xpath("//*[@name='email']").clear()
     driver.find_element_by_xpath("//*[@name='email']").send_keys("15309215554")
-    driver.find_element_by_css_selector("[name=password]").send_keys("616387521")
+    driver.find_element_by_css_selector("[name='password']").send_keys("616387521")
     driver.find_element_by_css_selector("#dologin").click()
+
+def logout():
+    driver.find_element_by_link_text("退出")
+
+driver = webdriver.Chrome()
+driver.get("http://mail.163.com")
+
+login()
+time.sleep(3)
+logout()
+driver.quit()
